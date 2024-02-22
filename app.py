8c9cec80eacde52f037ecdc04755ff49cf107933
Kakeru Uda <kakelu0713@gmail.com>
2024-02-22 22:37:28 +0900

    HTMLを正常に表示させました

--- app.py
+++ app.py
@@ -3,13 +3,37 @@ import sqlite3
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def hello():
-#     return redirect("/index")
-
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/oyakabuadd")
+def oyakabuadd():
+    return render_template("oyakabu-add.html")
+
+@app.route("/kouhaiadd")
+def kouhaiadd():
+    return render_template("kouhai-add.html")
+
+@app.route("/kouhaikokabuadd")
+def kouhaikokabuadd():
+    return render_template("kouhai-kokabu-add.html")
+
+@app.route("/syousai")
+def syousai():
+    return render_template("syousai.html")
+
+@app.route("/yearlist")
+def yearlist():
+    return render_template("year-list.html")
+
+@app.route("/yearlist/datelist")
+def datalist():
+    return render_template("date-list.html")
+
+@app.route("/yearlist/datelist/kouhailist")
+def kouhailist():
+    return render_template("kouhai-list.html")
 
 if __name__ == "__main__":
     app.run(port=8888, debug=False)
