3c7fe47499f0b1ab2a791acc07e292a9b93f1699
北山 裕也 <yuyaki_koyake@yahoo.co.jp>
2024-02-22 21:32:29 +0900

    day1

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session
+import sqlite3
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    hello = "Hello world"
-    return hello
+# @app.route('/')
+# def hello():
+#     return redirect("/index")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=8888, debug=False)
