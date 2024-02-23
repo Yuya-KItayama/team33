a9b8938a9b49387feaf5f666074e92ccf367386c
Kakeru Uda <kakelu0713@gmail.com>
2024-02-23 19:56:28 +0900

    親株登録のバックエンド制作

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session , url_for
+import os
 import sqlite3
+import datetime
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -7,13 +10,52 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+UPLOAD_FOLDER = 'FlowerImage'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @app.route("/oyakabuadd")
 def oyakabuadd():
     return render_template("oyakabu-add.html")
 
+@app.route('/OyakabuUpload', methods=['POST'])
+def OyakabuUpload():
+    if 'image' not in request.files:
+        return redirect("/oyakabuadd")
+    
+    file = request.files['image']
+
+    if file.filename == '':
+        return redirect("/oyakabuadd")
+    
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        color = request.form.get("color")
+        pattern = request.form.get("pattern")
+        blooming = request.form.get("blooming")
+        shape = request.form.get("shape")
+        remarks = request.form.get("remarks")
+        tgid = request.form.get("tgid")
+        sex = request.form.get("sex")
+        date = datetime.date.today()
+        print(color,pattern,blooming,shape,remarks,sex)
+        conn = sqlite3.connect("OyaKabu.db")
+        c = conn.cursor()
+        c.execute("insert into oyakabu values(null,?,?,?,?,?,?,?,?,?)",(color,pattern,blooming,shape,date,filename,tgid,remarks,sex))
+        conn.commit()
+        c.close()
+        # ここでデータベースにカテゴリと備考を保存する処理を追加
+    
+    return redirect("/oyakabuadd")
+
 @app.route("/kouhaiadd")
 def kouhaiadd():
+    conn = sqlite3.connect("myTask.db")
+    c = conn.cursor()
+    c.execute("select tg from oyakabu where sex = 0")
+    
     return render_template("kouhai-add.html")
+
 
 @app.route("/kouhaikokabuadd")
 def kouhaikokabuadd():
